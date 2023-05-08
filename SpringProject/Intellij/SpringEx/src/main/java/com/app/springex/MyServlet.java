420f8852ee6875704998bb26ced94fd4965f7cc9
JYLEE0110 <128339100+JYLEE0110@users.noreply.github.com>
2023-05-08 15:52:28 +0900

    Delete SpringProject/Intellij/SpringEx directory

--- SpringProject/Intellij/SpringEx/src/main/java/com/app/springex/MyServlet.java
+++
@@ -1,9 +0,0 @@
-package com.app.springex;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
-@WebServlet
-public class MyServlet extends HttpServlet {
-    
-}
